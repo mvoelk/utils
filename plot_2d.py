8700b826921be8dc6cd45f5383d2c940c40628f7
Markus Völk <markus.voelk@gmx.de>
2023-02-03 13:24:13 +0100

    add function for plotting histogram of prediction error

--- plot_2d.py
+++ plot_2d.py
@@ -7,6 +7,7 @@ Code was taken from https://github.com/mvoelk/utils
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def bar_plot(labels, data, title=None, total_width=.8, single_width=.9, vertical=False):
@@ -48,3 +49,43 @@ def bar_plot(labels, data, title=None, total_width=.8, single_width=.9, vertical
 
     plt.title(title)
     plt.show()
+
+
+def plot_abs_error_hist(y_true, y_pred=None, mask=None, bins=100, cumulative=False, name='', x_max=None, y_max=None):
+    """Plots the histogram of the absolute prediction error.
+
+    # Notes
+        y_true and y_pred are flattened
+        mask has to be of same shape
+
+    # Example 1
+        mask = y_true[i,...,0] < 0.5
+        plot_abs_error_hist(y_true[i,...,1], y_pred[i,...,1], bins=100, y_max=100, cumulative=False)
+        plot_abs_error_hist(y_true[i,...,1], y_pred[i,...,1], mask, bins=100, y_max=100, cumulative=False)
+    # Example 2
+        mask = y_true[i,...,0] < 0.5
+        plot_abs_error_hist(y_true[i,...,1], y_pred[i,...,1], bins=100, y_max=None, cumulative=True)
+        plot_abs_error_hist(y_true[i,...,1], y_pred[i,...,1], mask, bins=100, y_max=None, cumulative=True)
+    """
+    
+    y_true = np.reshape(np.float32(y_true), (-1))
+    
+    if y_pred is not None:
+        y_pred = np.reshape(np.float32(y_pred), (-1))
+    else:
+        y_pred = 0
+    
+    abs_err = np.abs(y_true-y_pred)
+
+    if mask is not None:
+        mask = np.reshape(np.bool8(mask), (-1))
+        abs_err = abs_err[mask]
+
+    plt.figure(figsize=(16,4))
+    sns.histplot(abs_err, kde=True, bins=bins, cumulative=cumulative)
+    plt.ylim(0, y_max); plt.xlim(0, x_max)
+    plt.axvline(x=np.mean(abs_err), color='red')
+    sparsity = 1-len(abs_err)/len(y_true)
+    plt.title(name + (' (sparsity %.2f)'%(sparsity) if sparsity > 0 else ''))
+    plt.show()
+
