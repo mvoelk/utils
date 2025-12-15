2aefb6f6fae2940f275f9f32386fc095fd2d0701
Völk, Markus <markus.voelk@ipa.fraunhofer.de>
2025-12-15 15:43:27 +0100

    refactoring metrics, add covariance and correlation matrix

--- metrics.py
+++ metrics.py
@@ -1,13 +1,36 @@
 """
 SPDX-License-Identifier: MIT
-Copyright © 2017 - 2022 Markus Völk
+Copyright © 2017 - 2025 Markus Völk
 Code was taken from https://github.com/mvoelk/utils
 """
 
 import numpy as np
 
 
-def calc_similarity_matrix(z):
+def covariance_matrix(z):
+    """Covariance of the features in a batch
+
+    # Arguments
+        z: shape (num_samples, num_features)
+    """
+    z = z - np.mean(z, axis=0)
+    return np.sum( z[:,:,None] @ z[:,None,:], axis=0) / (z.shape[0] - 1)
+
+def correlation_matrix(z):
+    """Correlation of the features in a batch
+
+    # Arguments
+        z: shape (num_samples, num_features)
+    """
+    z = z - np.mean(z, axis=0)
+    cov = np.sum( z[:,:,None] @ z[:,None,:], axis=0) / (z.shape[0] - 1)
+    std = np.sqrt(np.diag(cov))
+    std_outer = std[:,None] @ std[None,:]
+    corr = cov / std_outer
+    corr[std_outer==0] = 0  # handle zero-variance
+    return corr
+
+def similarity_matrix(z):
     """Cosine similarity between a batch of vectors
 
     # Arguments
@@ -16,7 +39,7 @@ def calc_similarity_matrix(z):
     zn = np.linalg.norm(z, axis=-1)
     return (z @ z.T) / (zn[:,None] @ zn[None,:])
 
-def calc_distance_matrix(z):
+def distance_matrix(z):
     """Euclidean distance between a batch of vectors
 
     # Arguments
@@ -25,28 +48,46 @@ def calc_distance_matrix(z):
     return np.linalg.norm(z[:,None,:] - z[None,:,:], axis=-1)
 
 
-def calc_cosine_similarity(vectors, vector):
-    """Cosine similarity between a batch of vectors and a given vector
+def cosine_similarity(vectors1, vectors2):
+    """Cosine similarity between batches of vectors or a batch and a vector
 
     # Arguments
-        vectors: shape (num_vectors, num_features)
-        vector: shape (num_features)
+        vectors1: shape (num_vectors, num_features)
+        vectors2: shape (num_vectors, num_features) or (num_features)
+
+    # Return
+        shape (num_vectors)
     """
-    vs, v = vectors, vector
+    vs, v = vectors1, vectors2
     vsn, vn = np.linalg.norm(vs, axis=-1), np.linalg.norm(v, axis=-1)
-    return (vs @ v) / (vsn * vn)
+    return np.sum((vs*v), axis=-1) / (vsn*vn)
 
-def calc_euclidean_distance(vectors, vector):
-    """Euclidean distance between a batch of vectors and a given vector
+def euclidean_distance(vectors1, vectors2):
+    """Euclidean distance between batches of vectors or a batch and a vector
 
     # Arguments
-        vectors: shape (num_vectors, num_features)
-        vector: shape (num_features)
+        vectors1: shape (num_vectors, num_features)
+        vectors2: shape (num_vectors, num_features) or (num_features)
+
+    # Return
+        shape (num_vectors)
     """
-    return np.linalg.norm(vectors - vector, axis=-1)
+    return np.linalg.norm(vectors1 - vectors2, axis=-1)
+
+def mean_absolute_difference(vectors1, vectors2):
+    """Mean absolute difference (MAD) between batches of vectors or a batch and a vector
+
+    # Arguments
+        vectors1: shape (num_vectors, num_features)
+        vectors2: shape (num_vectors, num_features) or (num_features)
+
+    # Return
+        shape (num_vectors)
+    """
+    return np.mean(np.abs(vectors1 - vectors2), axis=-1)
 
 
-def calc_iou(boxes, box, fix_points=True):
+def intersection_over_union(boxes, box, fix_points=True):
     """Intersection over union for a batch of axis-aligned bounding
     boxes with a given bounding box.
 
@@ -75,3 +116,8 @@ def calc_iou(boxes, box, fix_points=True):
     union = boxes_a + box_a - inter
 
     return inter / union
+
+
+# legacy stuff
+calc_similarity_matrix, calc_distance_matrix = similarity_matrix, distance_matrix
+calc_cosine_similarity, calc_euclidean_distance = cosine_similarity, euclidean_distance
